33aac5f0dda640969d5023a9bda495f33805c43c
alekhya527 <41730750+alekhya527@users.noreply.github.com>
2018-07-26 15:27:49 +0530

    Create program1.java

---
+++ program1.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=1;
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+			else if(a<0)
+			{
+				System.out.println("Negetive");
+			}
+			else
+			{
+				System.out.println("Zero");
+			}
+		}
+	}
