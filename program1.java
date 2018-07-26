b4304f2ed28fc4f36245428ef2fdcb26d0895784
alekhya527 <41730750+alekhya527@users.noreply.github.com>
2018-07-26 15:46:10 +0530

    Update program1.java

--- program1.java
+++ program1.java
@@ -10,7 +10,9 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		int a=1;
+		int a;
+		Scanner Sc=new Scanner(System.in);
+		a=Sc.nextInt();
 		if(a>0)
 		{
 			System.out.println("Positive");
